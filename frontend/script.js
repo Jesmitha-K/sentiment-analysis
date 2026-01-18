82f1c231a527f2e2afdb10b44769b0833ca1e09f
Jesmi <167542158+Jesmitha-K@users.noreply.github.com>
2026-01-18 18:11:51 +0530

    Update script.js fetch URL to Render backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,11 @@
 async function analyze() {
   const text = document.getElementById("text").value;
 
-  const res = await fetch("https://sentiment-analysis-wsvt.onrender.com/predict", {
+  const res = await fetch("https://sentiment-analysis-3-04uy.onrender.com/predict", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ text })
-  });
+});
 
   const data = await res.json();
   document.getElementById("result").innerText = data.sentiment;
